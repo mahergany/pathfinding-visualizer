51da58a8fecb35b1345e4d70d5803b88cc46f250
mahergany <mahmed.bscs22seecs@seecs.edu.pk>
2023-11-20 18:51:44 +0500

    Making grid and allocating start and end node

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -6,13 +6,55 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount() {
+        const nodes = []
+        for (let row = 0; row < 20; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                const currentNode = { //object to give every node properties
+                    col,
+                    row,
+                    isStart: row === 10 && col === 5,
+                    isFinish: row === 10 && col === 45,
+                };
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({ nodes })
     }
 
     render() {
-        return (<div>
-            <h1>hello world</h1>
-            <Node></Node>
-        </div>)
+
+        const { nodes } = this.state;
+
+        return (
+            <div className="grid">
+                {nodes.map((row, rowId) => {
+                    return (<div key={rowId}>
+                        {row.map((node, nodeId) => {
+                            const { isStart, isFinish } = node;
+                            return (
+                                <Node
+                                    key={nodeId}
+                                    isStart={isStart}
+                                    isFinish={isFinish}
+                                    test={'foo'}
+                                //test={'kappa'}
+                                >
+
+                                </Node>
+                            );
+                        })}
+                    </div>
+                    );
+                })}
+            </div>
+        );
     }
 }
